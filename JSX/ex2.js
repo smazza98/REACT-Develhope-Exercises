abe573977aafa8342d1d9ac5c599f3f094502d64
sm <sm@mail.com>
2022-12-27 15:20:26 +0100

    JSX

---
+++ JSX/ex2.js
@@ -0,0 +1,9 @@
+const hello = "Simo"
+
+const greet = (name) => {
+    return <h1> Hello, {name} </h1>;
+}
+
+<div>
+    {greet(hello)}
+</div>
---
+++ JSX/ex3.js
@@ -0,0 +1,8 @@
+const calc = (a, b) => {
+    const sum = a + b;
+    return <h2> The sum of the numbers is {sum} </h2>;
+}
+
+<div>
+    {calc(2, 2)}
+</div>
