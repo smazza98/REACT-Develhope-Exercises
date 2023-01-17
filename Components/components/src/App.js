71de97893eb39741a00671e4c7d2d2a9bf08c2c2
sm <sm@mail.com>
2023-01-17 13:08:46 +0100

    Components

---
+++ Components/components/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hello from './Hello';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+      </div>
+    );
+  }
+}
+
+export default App;
